670d96f0159a0d1d07681ab4fe7e7b18f1f6f8be
haiqing <haiqing@gogs.io>
2018-08-27 19:05:10 +0800

    fix

--- hello.py
+++ hello.py
@@ -16,89 +16,89 @@ def stock_get_daily():
     return df.to_json(orient='table')
 
 @app.route("/stock/get_quote", methods=['POST'])
-def stock_get_daily():
+def get_quote():
     df, msg = stock.get_quote(symbols=request.form['symbols'])
     return df.to_json(orient='table')
 
 @app.route("/stock/get_kline", methods=['POST'])
-def stock_get_daily():
+def get_kline():
     df, msg = stock.get_kline(symbol=request.form['symbol'], trade_date=request.form['trade_date'], period=request.form['period'])
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_adj_factor", methods=['POST'])
-def stock_get_daily():
+def get_adj_factor():
     df, msg = stock.get_adj_factor(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 @app.route("/stock/get_trade_detail", methods=['POST'])
-def stock_get_daily():
+def get_trade_detail():
     df, msg = stock.get_trade_detail(symbol=request.form['symbol'],trade_date=request.form['trade_date'])
     return df.to_json(orient='table')
 
 @app.route("/stock/get_index_list", methods=['POST'])
-def stock_get_daily():
+def get_index_list():
     df, msg = stock.get_index_list(market=request.form['market'])
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_index_component", methods=['POST'])
-def stock_get_daily():
+def get_index_component():
     df, msg = stock.get_index_component(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 @app.route("/stock/get_rzrq_info", methods=['POST'])
-def stock_get_daily():
+def get_rzrq_info():
     df, msg = stock.get_rzrq_info(market=request.form['market'],date=request.form['date'])
     return df.to_json(orient='table')
 
 @app.route("/stock/get_dividend", methods=['POST'])
-def stock_get_daily():
+def get_dividend():
     df, msg = stock.get_dividend(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 
 
 @app.route("/stock/get_pledge_info", methods=['POST'])
-def stock_get_daily():
+def get_pledge_info():
     df, msg = stock.get_pledge_info(market=request.form['market'],date=request.form['date'])
     return df.to_json(orient='table')
 
 
 
 @app.route("/stock/get_report_data", methods=['POST'])
-def stock_get_daily():
+def get_report_data():
     df, msg = stock.get_report_data(symbol=request.form['symbol'],type=request.form['type'])
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_shareholder_structure", methods=['POST'])
-def stock_get_daily():
+def get_shareholder_structure():
     df, msg = stock.get_shareholder_structure(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 
 
 @app.route("/stock/get_realtime_money_flow", methods=['POST'])
-def stock_get_daily():
+def get_realtime_money_flow():
     df, msg = stock.get_realtime_money_flow(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_hist_money_flow", methods=['POST'])
-def stock_get_daily():
+def get_hist_money_flow():
     df, msg = stock.get_hist_money_flow(symbol=request.form['symbol'])
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_hist_money_flow_market", methods=['POST'])
-def stock_get_daily():
+def get_hist_money_flow_market():
     df, msg = stock.get_hist_money_flow_market()
     return df.to_json(orient='table')
 
 
 @app.route("/stock/get_realtime_money_flow_market", methods=['POST'])
-def stock_get_daily():
+def get_realtime_money_flow_market():
     df, msg = stock.get_realtime_money_flow_market()
     return df.to_json(orient='table')
 
